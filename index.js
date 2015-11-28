8fe742571a003e5163b0ab762691111e411b68da
ほと <mail@hoto.me>
2015-11-28 23:37:16 +0900

    handle error

--- index.js
+++ index.js
@@ -5,8 +5,10 @@ module.exports = function(robot) {
   robot.receiveMiddleware(function(context, next, done) {
     var dicPath = process.env.HUBOT_KUROMOJI_DICTIONARY_PATH || 'node_modules/kuromoji/dist/dict/';
     kuromoji.builder({ dicPath: dicPath }).build(function(err, tokenizer) {
-      if (err) { throw err; }
-      if ( context.response.message.tokenized === undefined ) {
+      if ( err ) {
+        robot.logger.error(err);
+      }
+      else if ( context.response.message.tokenized === undefined ) {
         var tokenized = tokenizer.tokenize( context.response.message.text );
         context.response.message.tokenized = tokenized;
       }
