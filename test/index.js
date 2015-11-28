cfd62858abca6c04c61d4aba69e012b02b018ca5
ほと <mail@hoto.me>
2015-11-29 00:31:28 +0900

    test

---
+++ test/index.js
@@ -0,0 +1,32 @@
+var assert = require('assert');
+
+var mockRobot = {
+  middleware: null,
+  receiveMiddleware: function(middleware) {
+    this.middleware = middleware;
+  },
+};
+
+describe('hubot', function() {
+
+  require('../')(mockRobot);
+
+  it('should register middleware', function() {
+    assert.ok( mockRobot.middleware );
+  });
+
+  it('should tokenize message', function(done) {
+    var context = { response: { message: { text: 'すもももももももものうち' } } };
+    mockRobot.middleware.apply(undefined, [
+      context,
+      function() {
+        var tokenized = context.response.message.tokenized;
+        assert.equal( tokenized[0].surface_form, 'すもも' );
+        assert.equal( tokenized[0].pos, '名詞' );
+        done();
+      },
+      function() {},
+    ]);
+  });
+
+});
