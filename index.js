87b5942f01cbd1656ca1fe05aefd29478b263957
ほと <mail@hoto.me>
2015-11-29 17:12:52 +0900

    bugfix

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 var kuromoji = require('kuromoji');
 
-var dicPath = process.env.HUBOT_KUROMOJI_DICTIONARY_PATH || __dirname + '/node_modules/kuromoji/dist/dict/';
-var tokenizer = null;
-kuromoji.builder({ dicPath: dicPath }).build(function(err, _tokenizer) {
-  if ( err ) {
-    robot.logger.error(err);
-  }
-  tokenizer = _tokenizer;
-});
-
 module.exports = function(robot) {
+
+  var dicPath = process.env.HUBOT_KUROMOJI_DICTIONARY_PATH || __dirname + '/node_modules/kuromoji/dist/dict/';
+  var tokenizer = null;
+  kuromoji.builder({ dicPath: dicPath }).build(function(err, _tokenizer) {
+    if ( err ) {
+      robot.logger.error(err);
+    }
+    tokenizer = _tokenizer;
+  });
+
   robot.receiveMiddleware(function(context, next, done) {
-    if ( tokenizer && context.response.message.tokenized === undefined ) {
-      var tokenized = tokenizer.tokenize( context.response.message.text );
-      context.response.message.tokenized = tokenized;
+    var message = context.response.message;
+    if ( tokenizer && message.tokenized === undefined && message.text ) {
+      message.tokenized = tokenizer.tokenize( message.text );
     }
     next();
   });
